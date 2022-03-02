cd60e79a410e7384951d853539ba3137dae4edfa
sayeef-rahman <sayeef.cse@gmail.com>
2022-03-02 20:42:31 +0600

    final submission

--- js/app.js
+++ js/app.js
@@ -14,6 +14,10 @@ const searchPhoe = () => {
     const finalSearch= searchText.toLowerCase();
     //console.log(finalSearch);
     searchField.value = '';
+    const searchResult = document.getElementById('search-result');
+    searchResult.innerHTML= '';
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.innerHTML = '';
     const div= document.getElementById('error-message');
     div.innerHTML="";
     const url= `https://openapi.programming-hero.com/api/phones?search=${finalSearch}`;
@@ -32,20 +36,14 @@ const searchPhoe = () => {
 };
 
 const displaySearchResult = data =>{
-    
+    //console.log(data.length);
     const searchResult = document.getElementById('search-result');
     searchResult.innerHTML="";
-    
-    // Show Details for Every Phone
-    // console.log("in show",data);
-    // for(let i=0; i < data.length; i++){
-    //     console.log("slug-",i,": ",data[i].slug);
-    // }
 
+    let phoneCount = 0;
     //Main Display Done
     data.forEach(phone => {
         let phoneId = phone.slug;
-        //console.log(phoneId);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML= `
@@ -59,8 +57,9 @@ const displaySearchResult = data =>{
       </div>
       `;
     searchResult.appendChild(div);
-
+    
     })
+    
 }
 
 const showDetails = id =>{
@@ -73,7 +72,31 @@ const showDetails = id =>{
 }
 
 const displayPhoneDetail = data => {
-    console.log(data);
+    //console.log(data);
+    const releaseDate = data.releaseDate;
+    const chipSet = data.mainFeatures.chipSet;
+    const display = data.mainFeatures.displaySize;
+    const memorySize = data.mainFeatures.memory;
+    const sensors = data.mainFeatures.sensors;
+    const storage = data.mainFeatures.storage;
+    //other features
+    const waln = data.others.WLAN;
+    const bluetooth = data.others.Bluetooth;
+    const gps = data.others.GPS;
+    const nfc = data.others.NFC;
+    const radio = data.others.Radio;
+    const usb = data.others.USB;
+
+    //console.log(data.others.WLAN);
+    const checkReleaseDate = text =>{
+        if(text == '' || text == undefined){
+            return 'Not Found!';
+        }
+        else{
+            return text;
+        }
+    }
+
     const phoneDetails = document.getElementById('phone-details');
     const searchResult = document.getElementById('search-result');
     searchResult.innerHTML=''; 
@@ -81,12 +104,71 @@ const displayPhoneDetail = data => {
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
-    <h1 class="p-3 text-center text-primary">Detils Information</h1>
-        <h5 class="card-title fs-1">${data.name}</h5>
-        <img src="${data.image}" class="card-img-top w-50 mx-auto mt-2 mb-3" alt="...">
-    <p><a href="" class="btn btn-primary">Buy Now</a></p>
+        <h1 class="p-3 text-center text-primary">Detail Information</h1>
+        <h5 class="card-title fs-1 text-center">${data.name}</h5>
+        <img src="${data.image}" class="row w-50 mx-auto mt-3 mb-3" alt="...">
+        <table class="table table-hover w-100">
+            <tbody>
+                <tr>
+                    <td class="border text-center m-auto w-50 fw-bold">Release Date:</td>
+                    <td class="border">${checkReleaseDate(releaseDate)}</td>
+                </tr>
+                <tr>
+                    <td class="border text-center m-auto w-50 fw-bold">Processor:</td>
+                    <td class="border">${checkReleaseDate(chipSet)}</td>
+                </tr>
+                <tr>
+                    <td class="border text-center m-auto w-50 fw-bold">Display:</td>
+                    <td class="border">${checkReleaseDate(display)}</td>
+                </tr>
+                <tr>
+                    <td class="border text-center m-auto w-50 fw-bold">Memory:</td>
+                    <td class="border">${checkReleaseDate(memorySize)}</td>
+                </tr>
+                <tr>
+                    <td class="border text-center m-auto w-50 fw-bold">Sensors:</td>
+                    <td class="border">${checkReleaseDate(sensors)}</td>
+                </tr>
+                <tr>
+                    <td class="border text-center m-auto w-50 fw-bold">Storage:</td>
+                    <td class="border">${checkReleaseDate(storage)}</td>
+                </tr>
+                <tr>
+                    <td class="border text-center m-auto w-50 fw-bold">Others:</td>
+                    <td class="border">
+                        <span class="fw-bold">WALN:</span> <span>${checkReleaseDate(waln)}</span><br>
+                        <span class="fw-bold">Bluetooth:</span> <span>${checkReleaseDate(bluetooth)}</span><br>
+                        <span class="fw-bold">GPS:</span> <span>${checkReleaseDate(gps)}</span><br>
+                        <span class="fw-bold">NFC:</span> <span>${checkReleaseDate(nfc)}</span><br>
+                        <span class="fw-bold">Radio:</span> <span>${checkReleaseDate(radio)}</span><br>
+                        <span class="fw-bold">USB:</span> <span>${checkReleaseDate(usb)}</span><br>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <p class="row w-50 text-center mx-auto"><a href="" class="btn btn-primary fs-2">Buy Now</a></p>
     `;
     phoneDetails.appendChild(div);
 }
 
-{/* <p class="card-text">${meal.strInstructions.slice(0, 150)}</p> */}
+/*
+
+data.forEach(phone => {
+    let phoneId = phone.slug;
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML= `
+    <div class="card shadow">
+        <img src="${phone.image}" class="card-img-top w-50 mx-auto mt-2 mb-3" alt="...">
+        <p class="bg-dark rounded-pill p-2 text-light mx-3 text-center w-50 mx-auto">${phone.brand}</p>
+        <div class="card-body">
+            <h5 class="card-title mb-3">${phone.phone_name}</h5>
+            <button class="btn btn-primary" onclick="showDetails('${phoneId}')">Show Details</button>
+        </div>
+  </div>
+  `;
+searchResult.appendChild(div);
+
+})
+
+*/
